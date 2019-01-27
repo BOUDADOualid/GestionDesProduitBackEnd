b9c6b3433ee7a051a909810fa84cbf7eb3633f57
Your Name <you@example.com>
2019-01-27 13:00:30 +0000

    first commit

---
+++ src/main/java/org/back/GestionDesProduitBackApplication.java
@@ -0,0 +1,14 @@
+package org.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesProduitBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDesProduitBackApplication.class, args);
+	}
+
+}
+
